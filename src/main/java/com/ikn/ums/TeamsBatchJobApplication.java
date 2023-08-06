04bcace3b9af6915363c5ee5826e45ab6a764424
Vaishnav Puthiyanamparambath <vaishnav.p@ikcontech.com>
2023-08-06 18:33:20 +0530

    updated main class

--- src/main/java/com/ikn/ums/TeamsBatchJobApplication.java
+++ src/main/java/com/ikn/ums/TeamsBatchJobApplication.java
@@ -2,9 +2,17 @@ package com.ikn.ums;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TeamsBatchJobApplication {
+public class TeamsBatchJobApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(TeamsBatchJobApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TeamsBatchJobApplication.class, args);
